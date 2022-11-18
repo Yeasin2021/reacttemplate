24b53fc313d4499629505efa20a6708a8fb11cf2
unknown <yeasin18801@gmail.com>
2022-11-18 12:33:06 +0600

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div>
       <Header />
+      {/* <AboutMe /> */}
     </div>
   );
 }
