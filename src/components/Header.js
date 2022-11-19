6d7c6dc8f8aafd9d517d5c2ab521a2882a4c2ce2
unknown <yeasin18801@gmail.com>
2022-11-19 18:18:00 +0600

    header json data dropdown-icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
         <li><a className="nav-link scrollto" href="#resume">Resume</a></li>
         <li><a className="nav-link scrollto" href="#services">Services</a></li>
         <li><a className="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-        <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
+        <li className="dropdown"><a href="#"><span>Drop Down</span> <i className={Data[0].dropdown_icon}></i></a>
           <ul>
             <li><a href="#">Drop Down 1</a></li>
             <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
