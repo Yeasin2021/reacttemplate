ac8af5e6bc2f032ccd7243776d1531b5f5f79093
unknown <yeasin18801@gmail.com>
2022-11-19 18:04:44 +0600

    header&banner dynamic-backgrond-image-url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import  Data  from '../data/Data'
 
 
 const Header = () => {
+  console.log(Data[0].image)
   return (
     <div className='header'>
         
@@ -40,10 +41,11 @@ const Header = () => {
   </header>
 
   
-  <section className="hero">
+  <section className="hero"  style={{ backgroundImage: `url(${(Data[0].image)})` }}>
     <div className="hero-container">
       <h1>Laura Thomson</h1>
       <h2>I'm a Professional Photographer In New York City</h2>
+      
       <a href="#about" className="btn-scroll scrollto" title="Scroll Down"><i className="bi bi-chevron-down"></i></a>
     </div>
   </section>
