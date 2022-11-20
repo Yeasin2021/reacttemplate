a5e33b34bcbd80f8390810c3d4949c7e6f068d00
unknown <yeasin18801@gmail.com>
2022-11-20 14:05:53 +0600

    Service-json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Resume from './components/Resume';
 import Service from './components/Service';
+import Testimonial from './components/Testimonial';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <AboutMe />
       <Resume />
       <Service />
+      {/* <Testimonial /> */}
       </div>
     </div>
   );
