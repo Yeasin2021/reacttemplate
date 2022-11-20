2ccfdc19326417914fe99f198f43ddfe01ca9dc4
unknown <yeasin18801@gmail.com>
2022-11-20 15:37:13 +0600

    Service-dynamic

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import ServiceData from '../data/Service.json'
-console.log(ServiceData)
+import  ServiceData  from '../data/Service.json'
+import ServiceDetails from './ServiceDetails'
+//console.log(ServiceData)
 const Service = () => {
   return (
     <div>
@@ -14,42 +15,31 @@ const Service = () => {
         </div>
 
         <div className="row">
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div className="icon-box">
-              <div className="icon"><i class="bi bi-balloon-heart-fill"></i></div>
-              <h4 className="title"><a href="">Lorem Ipsum</a></h4>
-              <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-            </div>
-          </div>
+            {/* {
+                ServiceData.map((Service,index) => <ServiceDetails key={index} props={Service} />)
+                //  <h1>{Service.service_title}
+            } */}
+            {
 
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div className="icon-box">
-              <div className="icon"><i class="bi bi-file-earmark-richtext"></i></div>
-              <h4 className="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div className="icon-box">
-              <div className="icon"><i class="bi bi-speedometer"></i></div>
-              <h4 className="title"><a href="">Magni Dolores</a></h4>
-              <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-          </div>
-
-          <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-            <div className="icon-box">
-              <div className="icon"><i class="bi bi-globe-asia-australia"></i></div>
-              <h4 className="title"><a href="">Nemo Enim</a></h4>
-              <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-            </div>
-          </div>
-
+                ServiceData.map((Service,index) => {
+                    return(
+                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                            <div className="icon-box">
+                            <div className="icon"><i class={Service.service_icon}></i></div>
+                            <h4 className="title"><a href="">{Service.service_title}</a></h4>
+                            <p className="description">{Service.service_description}</p>
+                            </div>
+                        </div> 
+                    )
+                })
+                
+            }
+    
         </div>
 
       </div>
     </section>
+    
     </div>
   )
 }
