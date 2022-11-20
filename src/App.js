c2b0818e2852867b6c0b2e4ad82aefa37e92ed01
unknown <yeasin18801@gmail.com>
2022-11-20 12:40:16 +0600

    Resume part

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Resume from './components/Resume';
+
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className='main'>
       <AboutMe />
+      <Resume />
       </div>
     </div>
   );
