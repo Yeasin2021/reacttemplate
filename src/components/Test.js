7584a87cbcb90c8d4dca345940aad4d11d088776
unknown <yeasin18801@gmail.com>
2022-11-21 17:26:34 +0600

    Resume data test-5

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import TestJson from '../data/Test.json';
+import TestJson2 from '../data/TestTwo.json';
+console.log(TestJson2)
 const Test = () => {
   return (
     <div>
       {
-        TestJson.map(testOne => (
+        // TestJson.map(testOne => (
+        //     <article>
+        //         <p>{testOne.id}</p>
+        //         <p>{testOne.name}</p>
+        //         <p>{testOne.class}</p>
+        //         {
+        //             testOne.media.map((item)=> <div>
+        //                 <p>{item.socialMedia}</p>
+        //                 <p>{item.videoMedia}</p>
+        //             </div>)
+        //         }
+        //     </article>
+        // ))
+        TestJson2.map(testOne => (
             <article>
-                <p>{testOne.id}</p>
-                <p>{testOne.name}</p>
-                <p>{testOne.class}</p>
                 {
                     testOne.media.map((item)=> <div>
-                        <p>{item.socialMedia}</p>
-                        <p>{item.videoMedia}</p>
+                        <p>{item.sm_title}</p>
+                        <p>{item.sm_key_word_one}</p>
+                        <p>{item.sm_details}</p>
+                        <p>{item.sm_address}</p>
+                        <p>{item.sm_phone}</p>
+                        <p>{item.sm_mail}</p>
+                        <p>{item.ed_title_one}</p>
                     </div>)
                 }
             </article>
