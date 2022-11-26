0dd858698daff8ed367e02f9cdd7e757bfde009e
unknown <yeasin18801@gmail.com>
2022-11-26 17:36:24 +0600

    ueseEffect use in testimonial 1 render 2

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -53,11 +53,11 @@ const Testimonial = () => {
                 <div className="swiper-slide">
                       <div className="testimonial-item">
                         <img src={item.image1} className="testimonial-img" alt="" />
-                        <h3>Saul Goodman</h3>
-                        <h4>Ceo & Founder</h4>
+                        <h3>{item.header1}</h3>
+                        <h4>{item.title1}</h4>
                         <p>
                           <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-                          Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                          {item.description1}
                           <i className="bx bxs-quote-alt-right quote-icon-right"></i>
                         </p>
                       </div>
