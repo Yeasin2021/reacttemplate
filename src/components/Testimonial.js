807911bf4d8254afb73772e6e21d1f574a2a69fc
unknown <yeasin18801@gmail.com>
2022-11-26 17:12:51 +0600

    ueseEffect use in testimonial section

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -41,7 +41,7 @@ const Testimonial = () => {
               <div className="testimonial-item">
                 <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img" alt="" />
                 <h3>Saul Goodman</h3>
-                <h4>Ceo &amp; Founder</h4>
+                <h4>Ceo & Founder</h4>
                 <p>
                   <i className="bx bxs-quote-alt-left quote-icon-left"></i>
                   Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
