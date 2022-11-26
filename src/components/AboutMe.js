fafe0543f2d299c17bcfb3ec972b562152079ed9
unknown <yeasin18801@gmail.com>
2022-11-27 00:26:19 +0600

    aboutme picture dynamic

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -28,7 +28,9 @@ const AboutMe = () => {
 
             <div className="row">
             {/* style={{ background:`url(${(item.image)})` }} */}
-              <div className="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+              <div className="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+                <img src={item.image} style={{ width:'100%',height:'auto' }} alt="" />
+              </div>
               <div className="col-lg-8 d-flex flex-column align-items-stretch">
                 <div className="content ps-lg-4 d-flex flex-column justify-content-center">
                   <div className="row">
