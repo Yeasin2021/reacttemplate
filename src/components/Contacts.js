bd78bf841e51bfe760080f161207dc51e43365db
unknown <yeasin18801@gmail.com>
2022-12-01 23:05:16 +0600

    remove 18

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -10,37 +10,36 @@ const Contacts = () => {
   },[])
   return (
     <div>
-      {/* {console.log(items)} */}
-      <section id="contact" class="contact">
-      <div class="container">
+      <section id="contact" className="contact">
+      <div className="container">
       {
         items && items.map((item,index)=>{
           return(
             <div key={index}>
-               <div class="section-title">
+               <div className="section-title">
           <span>Contact Me</span>
           <h2>Contact Me</h2>
           <p>{item.title}</p>
         </div>
 
-        <div class="row">
+        <div className="row">
 
-          <div class="col-lg-6">
+          <div className="col-lg-6">
 
-            <div class="row">
-              <div class="col-md-12">
-                <div class="info-box">
-                  <i class="bx bx-share-alt"></i>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="info-box">
+                  <i className="bx bx-share-alt"></i>
                   <h3>Social Profiles</h3>
-                  <div class="social-links">
+                  <div className="social-links">
                     {
                        item.icons.map((icon,index)=>(
                         <div key={index}>
-                            <a href="#" class="twitter"><i class={icon}></i></a>
-                            {/* <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                            <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a> */}
+                            <a href="#" className="twitter"><i className={icon}></i></a>
+                            {/* <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+                            <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+                            <a href="#" className="google-plus"><i className="bi bi-skype"></i></a>
+                            <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a> */}
                         </div>
                        ))
                     }
@@ -48,16 +47,16 @@ const Contacts = () => {
                   </div>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="bx bx-envelope"></i>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-envelope"></i>
                   <h3>Email Me</h3>
                   <p>{item.contactEmail}</p>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="bx bx-phone-call"></i>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-phone-call"></i>
                   <h3>Call Me</h3>
                   <p>{item.contactPhone}</p>
                 </div>
@@ -66,28 +65,28 @@ const Contacts = () => {
 
           </div>
 
-          <div class="col-lg-6">
-            {/* <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required />
+          <div className="col-lg-6">
+            {/* <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required />
+                <div className="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                 </div>
               </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required />
+              <div className="form-group mt-3">
+                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
               </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+              <div className="form-group mt-3">
+                <textarea className="form-control" name="message" rows="6" placeholder="Message" required></textarea>
               </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
+              <div className="my-3">
+                <div className="loading">Loading</div>
+                <div className="error-message"></div>
+                <div className="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              <div className="text-center"><button type="submit">Send Message</button></div>
             </form> */}
             <ContactMessage />
           </div>
@@ -97,40 +96,40 @@ const Contacts = () => {
           )
         })
       }
-        {/* <div class="section-title">
+        {/* <div className="section-title">
           <span>Contact Me</span>
           <h2>Contact Me</h2>
           <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p>
         </div>
 
-        <div class="row">
+        <div className="row">
 
-          <div class="col-lg-6">
+          <div className="col-lg-6">
 
-            <div class="row">
-              <div class="col-md-12">
-                <div class="info-box">
-                  <i class="bx bx-share-alt"></i>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="info-box">
+                  <i className="bx bx-share-alt"></i>
                   <h3>Social Profiles</h3>
-                  <div class="social-links">
-                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                    <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                  <div className="social-links">
+                    <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+                    <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+                    <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+                    <a href="#" className="google-plus"><i className="bi bi-skype"></i></a>
+                    <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
                   </div>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="bx bx-envelope"></i>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-envelope"></i>
                   <h3>Email Me</h3>
                   <p>contact@example.com</p>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4">
-                  <i class="bx bx-phone-call"></i>
+              <div className="col-md-6">
+                <div className="info-box mt-4">
+                  <i className="bx bx-phone-call"></i>
                   <h3>Call Me</h3>
                   <p>+1 5589 55488 55</p>
                 </div>
@@ -139,28 +138,28 @@ const Contacts = () => {
 
           </div>
 
-          <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required />
+          <div className="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required />
+                <div className="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
                 </div>
               </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required />
+              <div className="form-group mt-3">
+                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
               </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+              <div className="form-group mt-3">
+                <textarea className="form-control" name="message" rows="6" placeholder="Message" required></textarea>
               </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
+              <div className="my-3">
+                <div className="loading">Loading</div>
+                <div className="error-message"></div>
+                <div className="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              <div className="text-center"><button type="submit">Send Message</button></div>
             </form>
           </div>
 
