dca6937be0e55f5b8563f3d3c659f167b0e0db70
unknown <yeasin18801@gmail.com>
2022-12-01 23:04:18 +0600

    remove 17

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -11,35 +11,35 @@ const Pricing = () => {
 
   return (
     <div>
-    <section id="pricing" class="pricing">
-      <div class="container">
-        <div class="section-title">
+    <section id="pricing" className="pricing">
+      <div className="container">
+        <div className="section-title">
           <span>Pricing</span>
           <h2>Pricing</h2>
           <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p>
         </div>
 
-        <div class="row">
+        <div className="row">
 
-          {/* <div class="col-lg-3 col-md-6">
-            <div class="box">
+          {/* <div className="col-lg-3 col-md-6">
+            <div className="box">
               <h3>Free</h3>
               <h4><sup>$</sup>0<span> / month</span></h4>
               <ul>
                 <li>Aida dere</li>
                 <li>Nec feugiat nisl</li>
                 <li>Nulla at volutpat dola</li>
-                <li class="na">Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
+                <li className="na">Pharetra massa</li>
+                <li className="na">Massa ultricies mi</li>
               </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+              <div className="btn-wrap">
+                <a href="#" className="btn-buy">Buy Now</a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
-            <div class="box featured">
+          <div className="col-lg-3 col-md-6 mt-4 mt-md-0">
+            <div className="box featured">
               <h3>Business</h3>
               <h4><sup>$</sup>19<span> / month</span></h4>
               <ul>
@@ -47,16 +47,16 @@ const Pricing = () => {
                 <li>Nec feugiat nisl</li>
                 <li>Nulla at volutpat dola</li>
                 <li>Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
+                <li className="na">Massa ultricies mi</li>
               </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+              <div className="btn-wrap">
+                <a href="#" className="btn-buy">Buy Now</a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
-            <div class="box">
+          <div className="col-lg-3 col-md-6 mt-4 mt-lg-0">
+            <div className="box">
               <h3>Developer</h3>
               <h4><sup>$</sup>29<span> / month</span></h4>
               <ul>
@@ -66,15 +66,15 @@ const Pricing = () => {
                 <li>Pharetra massa</li>
                 <li>Massa ultricies mi</li>
               </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+              <div className="btn-wrap">
+                <a href="#" className="btn-buy">Buy Now</a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
-            <div class="box">
-              <span class="advanced">Advanced</span>
+          <div className="col-lg-3 col-md-6 mt-4 mt-lg-0">
+            <div className="box">
+              <span className="advanced">Advanced</span>
               <h3>Ultimate</h3>
               <h4><sup>$</sup>49<span> / month</span></h4>
               <ul>
@@ -84,8 +84,8 @@ const Pricing = () => {
                 <li>Pharetra massa</li>
                 <li>Massa ultricies mi</li>
               </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+              <div className="btn-wrap">
+                <a href="#" className="btn-buy">Buy Now</a>
               </div>
             </div>
           </div> */}
@@ -93,10 +93,10 @@ const Pricing = () => {
         {
           items && items.map((item,index)=>{
             return(
-              <div class="col-lg-3 col-md-6 mt-4 mt-md-0" key={index}>
-            <div class="box">
+              <div className="col-lg-3 col-md-6 mt-4 mt-md-0" key={index}>
+            <div className="box">
               {
-                (item.cardFee) >29 ? (<><span class="advanced">Advanced</span></>) : ''
+                (item.cardFee) >29 ? (<><span className="advanced">Advanced</span></>) : ''
               }
               <h3>{item.cardTitle}</h3>
               <h4><sup>$</sup>{item.cardFee}<span> / month</span></h4>
@@ -105,15 +105,15 @@ const Pricing = () => {
                 <li>{item.cardCourseTwo}</li>
                 <li>{item.cardCourseThree}</li>
                 {
-                  (item.cardFee) ==0 ? (<><li class="na">{item.cardCourseFour}</li></>) : (<><li>{item.cardCourseFour}</li></>)
+                  (item.cardFee) ==0 ? (<><li className="na">{item.cardCourseFour}</li></>) : (<><li>{item.cardCourseFour}</li></>)
                 }
                 {
-                  (item.cardFee) < 20 ? (<><li class="na">{item.cardCourseFive}</li></>) : (<><li>{item.cardCourseFive}</li></>)
+                  (item.cardFee) < 20 ? (<><li className="na">{item.cardCourseFive}</li></>) : (<><li>{item.cardCourseFive}</li></>)
                 }
                 
               </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
+              <div className="btn-wrap">
+                <a href="#" className="btn-buy">Buy Now</a>
               </div>
             </div>
           </div>
