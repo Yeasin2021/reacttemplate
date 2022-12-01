a4901f921a600f98b9717bc33e7d25f96fb88b80
unknown <yeasin18801@gmail.com>
2022-12-01 22:40:13 +0600

    remove 8

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AboutMe from './components/AboutMe';
 import Resume from './components/Resume';
 import Service from './components/Service';
 import Testimonial from './components/Testimonial';
-import Test from './components/Test';
 import Pricing from './components/Pricing';
 import Contacts from './components/Contacts';
 import Footer from './components/Footer';
@@ -20,7 +19,6 @@ function App() {
       <Service />
       <Testimonial />
       <Portfolio />
-      {/* <Test /> */}
       <Pricing />
       <Contacts />
       </div>
